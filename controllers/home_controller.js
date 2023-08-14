a0ea3d53e9789da40f6d965a953739816e0c523e
Shubham <shubham0707cse@gmail.com>
2023-08-14 11:46:25 +0530

    Done till showing posts and user of the post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,25 @@
-module.exports.home = function(req,res){
+const Post = require('../models/post');
+
+module.exports.home =async function(req,res){
     //console.log(req.cookies);
-    res.cookie('user_id',25);
-    return res.render('home',{title:"Home"});
+    //res.cookie('user_id',25);
+
+
+    // const posts = await Post.find({});
+    // return res.render('home',{
+    //     title : 'Codeial | Home',
+    //     posts: posts
+    // });
+
+    // populate the user of each post
+    const posts = await Post.find({}).populate('user').exec();
+    return res.render('home',{
+        title : 'Codeial | Home',
+        posts: posts
+    });
+
+
 }
 
 // module.exports.profile = function(req,res){
 //     return res.end('<h1> Welcome to profile page</h1>');
-// }
