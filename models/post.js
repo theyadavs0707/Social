f7e4ac30a28e679c448a3460497b2425afaa0010
Shubham <shubham0707cse@gmail.com>
2023-08-14 12:38:00 +0530

    Added comment Schema

--- models/post.js
+++ models/post.js
@@ -3,12 +3,19 @@ const  mongoose =  require('mongoose');
 const postSchema = new mongoose.Schema({
     content:{
         type: String,
-        reqiired: true
+        required: true
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    // include the array of ids of all comments in this post schema itself
+    comments:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'comment'
+        }
+    ]
 },{
     timestamps: true
 });
