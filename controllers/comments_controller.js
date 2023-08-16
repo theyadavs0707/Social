86eb2118c97091c910cd88196c1bd094d8d91a16
Shubham <shubham0707cse@gmail.com>
2023-08-16 19:37:39 +0530

    Done till deleting comments and divding home.ejs into partials

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -17,3 +17,17 @@ module.exports.create = async function(req,res){
     }
 
 }
+
+module.exports.destroy = async function(req,res){
+    const comment = await Comment.findById( req.params.id) ;
+    if(comment.user == req.user.id ){
+        let postId = comment.post ;
+        comment.deleteOne();
+        await Post.findByIdAndUpdate(postId , {$pull: {comments: req.params.id }});
+        return res.redirect('back');
+    }
+    else{
+        return res.redirect('back');
+    }
+
+}
