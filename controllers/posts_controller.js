1d8a95a9cee119274371ad66f2a98d150dd1a143
Shubham <shubham0707cse@gmail.com>
2023-08-16 12:29:16 +0530

    Delete a post and related comments

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 
 module.exports.create =async function(req,res){
@@ -7,4 +8,18 @@ module.exports.create =async function(req,res){
         user: req.user._id
     });
     return res.redirect('back');
+}
+
+
+module.exports.destroy =async  function(req,res){
+    let post = await Post.findById(req.params.id);
+    // .id means converting the object id into string
+    if(post.user == req.user.id){
+        post.deleteOne();
+        await Comment.deleteMany({ post:req.params.id });
+        return res.redirect('back');
+    }
+    else{
+        return res.redirect('back');
+    }
 }
